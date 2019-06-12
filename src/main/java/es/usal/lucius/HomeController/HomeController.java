4f5e34b57379236d17604defcec1fb02cb63ac6a
Sergio Pablo <sergiophernandez@gmail.com>
2019-06-13 00:14:13 +0200

    Configuracion de login

---
+++ src/main/java/es/usal/lucius/HomeController/HomeController.java
@@ -0,0 +1,14 @@
+package es.usal.lucius.HomeController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@GetMapping("/cargaInicial")
+    public String cargaInicial() {
+		return "Hola, aqui estoy";
+    }
+
+}
