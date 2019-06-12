533820aaef553089103581d009474ec59e2df173
Sergio Pablo <sergioph@usal.es>
2019-06-12 23:29:56 +0200

    Initial Commit

---
+++ src/main/java/es/usal/tfm/LuciusToolApplication.java
@@ -0,0 +1,13 @@
+package es.usal.tfm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuciusToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuciusToolApplication.class, args);
+	}
+
+}
