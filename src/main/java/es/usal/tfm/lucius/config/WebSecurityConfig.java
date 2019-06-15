ff0e9885493cd2e7e570c457d30892bfb04ac55f
Sergio <sergiophernandez@gmail.com>
2019-06-15 14:30:09 +0200

    LDAP funciona correctamente

--- src/main/java/es/usal/tfm/lucius/config/WebSecurityConfig.java
+++ src/main/java/es/usal/tfm/lucius/config/WebSecurityConfig.java
@@ -1,43 +1,42 @@
 package es.usal.tfm.lucius.config;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-            .authorizeRequests()
-                .antMatchers("/public/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-            .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
-            .logout()
-                .permitAll();
-    }
+	
+	@Value("${ldap.urls}")
+	private String ldapUrls;
+	@Value("${ldap.base.dn}")
+	private String ldapBaseDn;
+	@Value("${ldap.username}")
+	private String ldapSecurityPrincipal;
+	@Value("${ldap.password}")
+	private String ldapPrincipalPassword;
+	@Value("${ldap.user.dn.pattern}")
+	private String ldapUserDnPattern;
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/public/**").permitAll().anyRequest().authenticated().and().formLogin()
+				.loginPage("/login").permitAll().and().logout().permitAll();
+	}
 
-    @Bean
-    @Override
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-             User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("password")
-                .roles("USER")
-                .build();
-
-        return new InMemoryUserDetailsManager(user);
-    }
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+		.ldapAuthentication()
+		.contextSource()
+		.url(ldapUrls + ldapBaseDn)
+		.managerDn(ldapSecurityPrincipal)
+		.managerPassword(ldapPrincipalPassword)
+		.and()
+		.userDnPatterns(ldapUserDnPattern);
+	}
 }
