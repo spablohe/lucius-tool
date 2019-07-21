e0c4d1cb7d355a6f6ba1a08f88e6b0cfe1c84c5b
spablohe <spablohe@gmail.com>
2019-07-21 18:27:22 +0200

    Envio de email con formato HTML

--- src/main/java/es/usal/tfm/lucius/mail/controller/MailController.java
+++ src/main/java/es/usal/tfm/lucius/mail/controller/MailController.java
@@ -1,10 +1,18 @@
 package es.usal.tfm.lucius.mail.controller;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.mail.MessagingException;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import es.usal.tfm.lucius.mail.dto.EmailDto;
 import es.usal.tfm.lucius.mail.service.MailService;
 
 @RestController
@@ -17,10 +25,50 @@ public class MailController {
 		super();
 	}
 	
-	@RequestMapping(value="/sendMailTest", method=RequestMethod.GET)
-	public String sendMailTest() {
-		mailService.sendEmail();
-		return "ok";
+	@PostMapping(value = "/sendemail")
+	public EmailDto emailHtmlTemplate(@RequestBody EmailDto email) throws IOException, MessagingException {
+
+		mailService.sendEmail(email);
+
+		return email;
+	}
+
+	@PostMapping(value = "/sendtextemail")
+	public EmailDto emailTextTemplate(@RequestBody EmailDto email) throws IOException, MessagingException {
+
+		mailService.sendTextTemplateEmail(email);
+
+		return email;
+	}
+
+	@GetMapping(value = "/sendhtmlemail")
+	public EmailDto emailInHtmlTemplate() throws IOException, MessagingException {
+		EmailDto mail = new EmailDto();
+		mail.setFrom("spablohe@gmail.com");
+		mail.setHtml(true);
+		mail.setMessage("Este es un mail de prueba");
+		mail.setSubject("Mensaje de prueba");
+		mail.setTemplateName("email-inlineimage");
+		mail.setTemplate(true);
+		mail.setAttachmentName("");
+		mail.setEmailedMessage("emailedmessaged");
+		mail.setHasAttachment(false);
+		mail.setParameterMap(new HashMap<String,Object>());
+		mail.setStaticResourceMap(new HashMap<String,Object>());
+		String [] dest = {"sergiophernandez@gmail.com"};
+		mail.setTo(dest);
+		mailService.sendHtmlEmail(mail);
+
+		return mail;
+	}
+
+	@PostMapping(value = "/sendemails")
+	public Iterable<EmailDto> emailHtmlTemplate(@RequestBody List<EmailDto> emails)
+			throws IOException, MessagingException {
+
+		mailService.sendEmails(emails);
+
+		return emails;
 	}
 
 }
--- src/main/java/es/usal/tfm/lucius/mail/service/IMailService.java
+++ src/main/java/es/usal/tfm/lucius/mail/service/IMailService.java
@@ -1,13 +1,20 @@
 package es.usal.tfm.lucius.mail.service;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.mail.MessagingException;
 
+import es.usal.tfm.lucius.mail.dto.EmailDto;
+
 public interface IMailService {
 	
-	public void sendEmail();
+	public EmailDto sendEmail(EmailDto emailDto) throws MessagingException, IOException;
 	
-	public void sendEmailWithAttachment() throws MessagingException, IOException;
+	public EmailDto sendTextTemplateEmail(EmailDto emailDto) throws IOException, MessagingException;
+	
+	public EmailDto sendHtmlEmail(EmailDto emailDto) throws MessagingException, IOException;
+	
+	public List<EmailDto> sendEmails(List<EmailDto> emailDtos) throws MessagingException, IOException;
 
 }
