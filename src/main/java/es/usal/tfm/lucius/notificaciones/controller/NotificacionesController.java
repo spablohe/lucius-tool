5d1e950bd082147dc7f5758857f86baf457801e8
spablohe <spablohe@gmail.com>
2019-07-21 18:55:43 +0200

    Creacion del controller de notificaciones

---
+++ src/main/java/es/usal/tfm/lucius/notificaciones/controller/NotificacionesController.java
@@ -0,0 +1,10 @@
+package es.usal.tfm.lucius.notificaciones.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NotificacionesController {
+	
+	
+	
+}
