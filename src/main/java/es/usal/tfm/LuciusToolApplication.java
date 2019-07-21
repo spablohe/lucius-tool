9e2bd6bdd7f74025a81b52ba2d06c2acbe02659f
spablohe <spablohe@gmail.com>
2019-07-22 00:13:27 +0200

    Modificacion para las tareas periodicas y un ejemplo

--- src/main/java/es/usal/tfm/LuciusToolApplication.java
+++ src/main/java/es/usal/tfm/LuciusToolApplication.java
@@ -2,8 +2,10 @@ package es.usal.tfm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class LuciusToolApplication {
 
 	public static void main(String[] args) throws Throwable {
