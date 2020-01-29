bd723f94045993b73cc94fc8b817b2f3eb725d4d
spablohe <spablohe@gmail.com>
2020-01-29 22:58:15 +0100

    mailSender - Añadido Swagger para documentar el API

--- src/main/java/es/usal/tfm/LuciusToolApplication.java
+++ src/main/java/es/usal/tfm/LuciusToolApplication.java
@@ -1,13 +1,22 @@
 package es.usal.tfm;
 
+import es.usal.tfm.lucius.config.SwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 @SpringBootApplication
+@Import(SwaggerConfiguration.class)
 public class LuciusToolApplication {
 
 	public static void main(String[] args) throws Throwable {
 		SpringApplication.run(LuciusToolApplication.class, args);
+	}
+
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html")
+				.addResourceLocations("classpath:/META-INF/resources/");
 	}
 
 }
